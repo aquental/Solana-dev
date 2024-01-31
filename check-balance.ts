7b4ece7f25c0fed2d7071b2b5c42bac5a5d30c2c
Antonio Quental <aquental@users.noreply.github.com>
2024-01-31 16:24:24 -0300

    lesson two

--- check-balance.ts
+++ check-balance.ts
@@ -1,13 +1,21 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
 
-//try: 9TAiq2rgwPhfrksxFUtEtqCEra7qBZ3ZtkSgtzUCnZq9 (5)
+//try: 9TAiq2rgwPhfrksxFUtEtqCEra7qBZ3ZtkSgtzUCnZq9 (5 SOL)
+//toly.sol: 86xCnPeV69n6t3DnyGvkKobf9FdN2H9oiVDdaMpo2MMY
+//shaq.sol: gacMrsrxNisAhCfgsUAVbwmTC3w9nJB6NychLAnTQFv
+//mccann.sol: JCZjJcmuWidrj5DwuJBxwqHx7zRfiBAp6nCLq3zYmBxd
+
 var suppliedPublicKey = process.argv[2];
 if (!suppliedPublicKey) {
   suppliedPublicKey = "FzQZCvAQnyXMGnZEMgrbC4iHodtKxMXXCc42Vk7NGXua";
   console.log(`Using the default public key ${suppliedPublicKey}`);
 }
 
-const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+//const connection = new Connection("https://api.devnet.solana.com", "confirmed");
+const connection = new Connection(
+  "https://api.mainnet-beta.solana.com",
+  "confirmed"
+);
 
 const publicKey = new PublicKey(suppliedPublicKey);
 
